4afa060d35527fb51c08a3a54ba69ce7c293a4bf
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-01-31 11:48:29 +0545

    Add route_manager.dart to common_export.dart

--- lib/src/common/common_export.dart
+++ lib/src/common/common_export.dart
@@ -2,3 +2,4 @@ export 'package:catering_user_app/src/common/widgets/build_textField.dart';
 export 'package:catering_user_app/src/common/widgets/build_button.dart';
 export 'package:catering_user_app/src/common/helper/time_distance_function.dart';
 export 'package:catering_user_app/src/common/splash_screen.dart';
+export 'package:catering_user_app/src/common/route_manager.dart';
