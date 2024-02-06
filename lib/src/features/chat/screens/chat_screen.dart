279b044a1a9269d78a508ede725af46a8a72da80
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-02-06 22:06:32 +0545

    Add Dio package for sending push notifications

--- lib/src/features/chat/screens/chat_screen.dart
+++ lib/src/features/chat/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:catering_user_app/src/features/chat/data/chat_provider.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
@@ -9,10 +10,13 @@ import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 
-
 class ChatScreen extends StatelessWidget {
   final types.Room room;
-  const ChatScreen({super.key, required this.room});
+  final String token;
+  final String name;
+
+  const ChatScreen(
+      {super.key, required this.room, required this.token, required this.name});
 
   @override
   Widget build(BuildContext context) {
@@ -27,11 +31,30 @@ class ChatScreen extends StatelessWidget {
               builder: (context, snapshot) {
                 return Chat(
                   messages: snapshot.data ?? [],
-                  onSendPressed: (value) {
+                  onSendPressed: (value) async {
+                    final dio = Dio();
                     FirebaseChatCore.instance.sendMessage(value, room.id);
+                    try{
+                      await dio.post('https://fcm.googleapis.com/fcm/send', data: {
+                        "to": token,
+                        "priority": "High",
+                        "default_notification_channel_id": "high_importance_channel",
+                        "notification":{
+                          "body": value.text.trim(),
+                          "title":"New Message",
+                        },
+
+                      }, options: Options(
+                          headers:  {
+                            'Authorization': 'key=AAAAD8GZyWs:APA91bELjX2lfw6syAbhStEhvC-qJ3FOsVdZurTxwHMoIuALvz-Hz9pMw2OKhGCFgItEREtymGZgyn9YTe1Dp0oAnEyurWBLqK5M42MfeeKRF7vJZ7zBgX88MBQDM9txvoX8DGMDzAPm'
+                          }
+                      ));
+                    }catch(err){
+                      print(err);
+                    }
                   },
                   isAttachmentUploading: isLoad,
-                  onAttachmentPressed: () async{
+                  onAttachmentPressed: () async {
                     final result = await ImagePicker().pickImage(
                       imageQuality: 70,
                       maxWidth: 1440,
@@ -44,7 +67,9 @@ class ChatScreen extends StatelessWidget {
 
                       try {
                         final imageName = DateTime.now().toIso8601String();
-                        final reference = FirebaseStorage.instance.ref().child('chatImage/$imageName');
+                        final reference = FirebaseStorage.instance
+                            .ref()
+                            .child('chatImage/$imageName');
                         await reference.putFile(file);
                         final uri = await reference.getDownloadURL();
 
@@ -59,14 +84,18 @@ class ChatScreen extends StatelessWidget {
                           room.id,
                         );
                         ref.read(loadingProvider.notifier).toggle();
-                      } catch(err){
+                      } catch (err) {
                         ref.read(loadingProvider.notifier).toggle();
                       }
                     }
                   },
                   showUserAvatars: true,
                   showUserNames: true,
-                  user: types.User(id: FirebaseChatCore.instance.firebaseUser?.uid ?? ""),
+                  theme: const DefaultChatTheme(
+                    primaryColor: Color(0xff0084ff),
+                  ),
+                  user: types.User(
+                      id: FirebaseChatCore.instance.firebaseUser?.uid ?? ""),
                   useTopSafeAreaInset: true,
                 );
               },
