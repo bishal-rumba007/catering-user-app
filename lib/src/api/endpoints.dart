0f5f8e124fe78d83edcd2d2b23c8b76f51c811ef
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-02-07 16:16:46 +0545

    Add API keys and endpoints

---
+++ lib/src/api/endpoints.dart
@@ -0,0 +1,6 @@
+class ApiEndPoints {
+  static const String baseNotificationUrl =
+      'https://fcm.googleapis.com/fcm/send';
+  static const String notificationUrl =
+      'https://fcm.googleapis.com/v1/projects/cateringservicemgmt/messages:send';
+}
