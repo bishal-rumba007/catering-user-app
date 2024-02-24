0872c1ab62d41c9bacb2cbe54c17e1cbb793ead0
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-02-24 22:59:44 +0545

    Add separator widget

--- lib/src/common/common_export.dart
+++ lib/src/common/common_export.dart
@@ -3,3 +3,4 @@ export 'package:catering_user_app/src/common/widgets/build_button.dart';
 export 'package:catering_user_app/src/common/helper/time_distance_function.dart';
 export 'package:catering_user_app/src/common/splash_screen.dart';
 export 'package:catering_user_app/src/common/route_manager.dart';
+export 'package:catering_user_app/src/common/widgets/separator.dart';
