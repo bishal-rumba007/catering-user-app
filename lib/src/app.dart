35fd4b56f58712d22ae025c1e421706f52c421dc
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-02-24 22:59:21 +0545

    Add Khalti payment integration

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,6 +2,7 @@ import 'package:catering_user_app/src/common/common_export.dart';
 import 'package:catering_user_app/src/themes/export_themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
@@ -13,15 +14,28 @@ class MainApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(393, 873),
       builder: (context, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Flutter Demo',
-          theme: lightMode,
-          darkTheme: darkMode,
-          home: const SplashScreen(),
-          navigatorKey: navigatorKey,
-          onGenerateRoute: RouteGenerator.generateRoute,
-          initialRoute: Routes.splashRoute,
+        return KhaltiScope(
+          publicKey: 'test_public_key_599f6b5632ee4bfabc89b6d5dc55234d',
+          enabledDebugging: true,
+          builder: (context, navigatorKey) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: lightMode,
+              darkTheme: darkMode,
+              home: const SplashScreen(),
+              navigatorKey: navigatorKey,
+              onGenerateRoute: RouteGenerator.generateRoute,
+              initialRoute: Routes.splashRoute,
+              supportedLocales: const [
+                Locale('en', 'US'),
+                Locale('ne', 'NP'),
+              ],
+              localizationsDelegates: const [
+                KhaltiLocalizations.delegate,
+              ],
+            );
+          },
         );
       },
     );
