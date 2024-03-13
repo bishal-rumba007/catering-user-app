3fcc13429c5fbf31cf17ad27990cb773ad9bf0a4
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-03-13 21:35:04 +0545

    lock device orientation & update packages

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:catering_user_app/src/api/firebase_api.dart';
 import 'package:catering_user_app/src/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -14,6 +15,11 @@ Future<void> main() async {
   );
   await Hive.initFlutter();
   await FirebaseApi().initNotifications();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(
     const ProviderScope(
       child: MainApp(),
